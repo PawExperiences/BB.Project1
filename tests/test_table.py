cde1100168424b04993e236bfabe70ae303740ec
BuildBoard <buildboard@local>
2026-08-31 13:43:45 +0000

    feat: The numeral table

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,19 +1,20 @@
 import pytest
-from src.romans.table import NUMERAL_TABLE
+from romans.table import VALUE_TO_NUMERAL
 
-def test_numeral_table():
-    assert NUMERAL_TABLE == [
-        (1000, 'M'),
-        (900, 'CM'),
-        (500, 'D'),
-        (400, 'CD'),
-        (100, 'C'),
-        (90, 'XC'),
-        (50, 'L'),
-        (40, 'XL'),
-        (10, 'X'),
-        (9, 'IX'),
-        (5, 'V'),
-        (4, 'IV'),
-        (1, 'I'),
-    ]
+def test_value_to_numeral():
+    expected_pairs = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I',
+    }
+    assert VALUE_TO_NUMERAL == expected_pairs
